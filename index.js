be4fe24ae7a360d7ff2d7586147c37a2064e2e3f
Tiffany Man <tiffanyman19@gmail.com>
2024-01-09 16:37:44 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,52 +1,18 @@
 // Code your solution here
-const driversNames = ["Bobby", 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
-
-function findMatching( driversNames, driverName ) {
-   return driversNames.filter( ( driver ) => {
-      return driver.toLowerCase() === driverName.toLowerCase(); 
-  })
+function findMatching(array, targetName)
+{
+    const anotherArray = array.filter(arr => arr.toLowerCase() === targetName.toLowerCase());
+    return anotherArray;
 }
 
-console.log(findMatching(driversNames,"Bobby"))
-
-
-
-function fuzzyMatch(arr, keyword) {
-   return arr.filter(driver => {
-    let keywordLength = keyword.length // 3
-    let fuzzyWord = driver.substring(0, keywordLength) // Bob
-    return keyword.toLowerCase() === fuzzyWord.toLowerCase()
-
-  }) 
+function fuzzyMatch(array, letter)
+{
+    const anotherArray = array.filter(arr => arr.startsWith(letter));
+    return anotherArray;
 }
 
-fuzzyMatch(drivers, 'Sa');
-
-const drivers = [
-   {
-     name: 'Bobby',
-     hometown: 'Pittsburgh' },
-   {
-     name: 'Sammy',
-     hometown: 'New York' } ,
-   {
-     name: 'Sally',
-     hometown: 'Cleveland' },
-   {
-     name: 'Annette',
-     hometown: 'Los Angeles' },
-   {
-     name: 'Bobby',
-     hometown: 'Tampa Bay' }
- ];
-
-function matchName (arr1 , string){
-   const match = arr1.filter(obj => {
-      if(obj.name === string){
-         return obj;
-      }
-   })
-   return match;
+function matchName(array, targetName)
+{
+    const anotherArray = array.filter(arr => arr.name === targetName);
+    return anotherArray;
 }
-
-console.log(matchName(drivers, "Bobby"))
